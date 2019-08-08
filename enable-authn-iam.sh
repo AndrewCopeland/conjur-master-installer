64f1583282dbb8893f930df4d2289ba5731e3c22
AndrewCopeland <andcope1995@gmail.com>
2019-08-08 16:05:45 -0400

    moved to enable iam script

--- enable-authn-iam.sh
+++ enable-authn-iam.sh
@@ -1,37 +1,50 @@
 #!/bin/bash
 
+source ./config.env
+
 SERVICE_ID=$1
 AWS_ACCOUNT=$2
 IAM_ROLE_NAME=$3
 
 CONJUR_CONFIG_FILE="/opt/conjur/etc/conjur.conf"
 
+TEMPLATE_DIR="templates"
+TEMPLATE_AUTHN_IAM="$TEMPLATE_DIR/authn-iam.yaml"
+TEMPLATE_AUTHN_AWS_PORTAL="$TEMPLATE_DIR/aws-portal.yaml"
+TEMPLATE_AUTHN_GRANT="$TEMPLATE_DIR/authn-grant.yaml"
+
+POLICY_DIR="policies"
+POLICY_AUTHN_IAM="$POLICY_DIR/authn-iam.yaml"
+POLICY_AUTHN_AWS_PORTAL="$POLICY_DIR/aws-portal.yaml"
+POLICY_AUTHN_GRANT="$POLICY_DIR/authn-grant.yaml"
+
 if [ $# -ne 3 ]; then
     echo "Error invalid number of arguments"
-    echo "Usage: ./enable-iam.sh serviceId awsAccount Iam-Role-Name"
-    echo "Example: ./enable-iam.sh dev 56748839373318 ubuntu-conjur-iam"
+    echo "Usage: ./enable-authn-iam.sh serviceId awsAccount Iam-Role-Name"
+    echo "Example: ./enable-authn-iam.sh dev 56748839373318 ubuntu-conjur-iam"
     exit 1
 fi
 
 mkdir policies
 
 # Replace the placeholders
-sed "s/{{ SERVICE_ID }}/$SERVICE_ID/g" templates/authn-iam.yaml.template > ./policies/authn-iam.yaml
+sed "s/{{ SERVICE_ID }}/$SERVICE_ID/g" $TEMPLATE_AUTHN_IAM > $POLICY_AUTHN_IAM
 
-sed "s/{{ AWS_ACCOUNT }}/$AWS_ACCOUNT/g" templates/cust-portal.yaml.template | 
-    sed "s/{{ IAM_ROLE_NAME }}/$IAM_ROLE_NAME/g" > ./policies/cust-portal.yaml
+sed "s/{{ AWS_ACCOUNT }}/$AWS_ACCOUNT/g" $TEMPLATE_AUTHN_AWS_PORTAL | 
+    sed "s/{{ IAM_ROLE_NAME }}/$IAM_ROLE_NAME/g" > $POLICY_AUTHN_AWS_PORTAL
 
-sed "s/{{ AWS_ACCOUNT }}/$AWS_ACCOUNT/g" templates/authn-grant.yaml.template | 
+sed "s/{{ AWS_ACCOUNT }}/$AWS_ACCOUNT/g" $TEMPLATE_AUTHN_GRANT | 
     sed "s/{{ IAM_ROLE_NAME }}/$IAM_ROLE_NAME/g" |
-    sed "s/{{ SERVICE_ID }}/$SERVICE_ID/g" > ./policies/authn-grant.yaml
+    sed "s/{{ SERVICE_ID }}/$SERVICE_ID/g" > $POLICY_AUTHN_AWS_PORTAL
 
 # Copy policies to conjur cli
 docker cp ./policies conjur-cli:/policies
 
 # Load each of the policies
-docker exec conjur-cli conjur policy load root /policies/authn-iam.yaml
-docker exec conjur-cli conjur policy load root /policies/cust-portal.yaml
-docker exec conjur-cli conjur policy load root /policies/authn-grant.yaml
+conjur-cli 
+docker exec conjur-cli conjur policy load root $POLICY_AUTHN_AWS_PORTAL
+docker exec conjur-cli conjur policy load root $POLICY_AUTHN_AWS_PORTAL
+docker exec conjur-cli conjur policy load root $POLICY_AUTHN_AWS_PORTAL
 
 # update the conjur master environment variables
 res=$(docker exec conjur-master cat $CONJUR_CONFIG_FILE | grep "CONJUR_AUTHENTICATORS")
@@ -47,3 +60,15 @@ fi
 
 # Restart conjur master
 docker exec conjur-master sv restart conjur
+
+
+# Get the environment variables needed for AWS
+echo "Environment Variables for AWS instance"
+
+cat << EOF
+export CONJUR_APPLIANCE_URL=https://$CONJUR_MASTER_NAME
+export AUTHN_IAM_SERVICE_ID=$SERVICE_ID
+export CONJUR_AUTHN_LOGIN=host/aws-portal/$AWS_ACCOUNT/$IAM_ROLE_NAME
+export CONJUR_CERT_FILE=./conjur-$CONJUR_ACCOUNT.pem
+export CONJUR_ACCOUNT=$CONJUR_ACCOUNT
+EOF
