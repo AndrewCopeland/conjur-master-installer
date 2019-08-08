068c5d13c0cfc191f0daf74d82097e8450f3711e
AndrewCopeland <andcope1995@gmail.com>
2019-08-08 17:36:58 -0400

    remove policies

--- aws-script.py
+++ aws-script.py
@@ -1,26 +1,12 @@
 from conjur_iam_client import *
 
-def get_all_variables(resources):
-    for resource in resources:
-        if ":variable:" in resource:
-            secret_id=resource.split(":", 3)[2]
-            return secret_id
-
-
 conjur_client = create_conjur_iam_client_from_env()
 
 resources=conjur_client.list()
 print(resources)
 
-all_secrets = get_all_variables(resources)
+conjur_client.set("aws-portal/database/username", "Username")
+conjur_client.set("aws-portal/database/password", "P@55W0RD123")
 
-# Setting conjur secrets
-for secret in all_secrets:
-    if "username" in secret:
-        conjur_client.set(secret, "Username")
-    if "password" in secret:
-        conjur_client.set(secret, "P@55W0RD123")
-
-# fetching the conjur secrets
-for secret in all_secrets:
-    print("{}: {}".format(secret, conjur_client.get(secret)))
+print(conjur_client.get("aws-portal/database/username"))
+print(conjur_client.get("aws-portal/database/password"))
